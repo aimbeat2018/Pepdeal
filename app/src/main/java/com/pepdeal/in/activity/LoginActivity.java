a3664166861b6b47d4a94820b64b3c318442e9dc
Nutan PC <nutan.aimbeat@gmail.com>
2021-11-24 18:33:40 +0530

    24/11/2021

--- app/src/main/java/com/pepdeal/in/activity/LoginActivity.java
+++ app/src/main/java/com/pepdeal/in/activity/LoginActivity.java
@@ -1,16 +1,44 @@
 package com.pepdeal.in.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.pepdeal.in.R;
+import com.pepdeal.in.databinding.ActivityLoginBinding;
+
+import es.dmoral.toasty.Toasty;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        binding.setHandler(new ClickHandler());
+    }
+
+    public class ClickHandler {
+        public ClickHandler() {
+        }
+
+        public void registerClick(View view) {
+            startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+        }
+
+        public void onLogin(View view) {
+            if (binding.edtMobileNo.getText().toString().equals("")||binding.edtMobileNo.getText().length()!=10) {
+                Toasty.info(LoginActivity.this, "Enter valid mobile number", Toasty.LENGTH_SHORT, true).show();
+            } else if (binding.edtPassword.getText().toString().equals("")) {
+                Toasty.info(LoginActivity.this, "Enter valid password", Toasty.LENGTH_SHORT, true).show();
+            } else {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/pepdeal/in/activity/RegistrationActivity.java
@@ -0,0 +1,45 @@
+package com.pepdeal.in.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.pepdeal.in.R;
+import com.pepdeal.in.databinding.ActivityRegistrationBinding;
+
+import es.dmoral.toasty.Toasty;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    ActivityRegistrationBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_registration);
+
+    }
+
+
+    public class ClickHandler {
+        public ClickHandler() {
+        }
+
+        public void onRegistration(View view) {
+            if (binding.edtName.getText().toString().equals("")) {
+                Toasty.info(RegistrationActivity.this, "Enter name to continue", Toasty.LENGTH_SHORT, true).show();
+            } else if (binding.edtMobileNo.getText().toString().equals("")||binding.edtMobileNo.getText().length()!=10) {
+                Toasty.info(RegistrationActivity.this, "Enter valid mobile number", Toasty.LENGTH_SHORT, true).show();
+            } else if (binding.edtPassword.getText().toString().equals("")) {
+                Toasty.info(RegistrationActivity.this, "Enter valid password", Toasty.LENGTH_SHORT, true).show();
+            }else if (binding.edtPassword.getText().toString().equals(binding.edtConfirmPassword.getText().toString())) {
+                Toasty.info(RegistrationActivity.this, "Both password should match", Toasty.LENGTH_SHORT, true).show();
+            } else {
+                startActivity(new Intent(RegistrationActivity.this, HomeActivity.class));
+            }
+        }
+    }
+}
